6baf663c91165ef32e85b46f171e82d5f7ff61dc
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2019-06-26 08:46:46 +0300

    Sync with v7.65.1

--- include/curl/curlver.h
+++ include/curl/curlver.h
@@ -30,7 +30,7 @@
 
 /* This is the version number of the libcurl package from which this header
    file origins: */
-#define LIBCURL_VERSION "7.65.1-DEV"
+#define LIBCURL_VERSION "7.65.1"
 
 /* The numeric version number is also available "in parts" by using these
    defines: */
@@ -68,7 +68,7 @@
  *
  * "2007-11-23"
  */
-#define LIBCURL_TIMESTAMP "2019-03-27"
+#define LIBCURL_TIMESTAMP "2019-06-05"
 
 #define CURL_VERSION_BITS(x,y,z) ((x)<<16|(y)<<8|(z))
 #define CURL_AT_LEAST_VERSION(x,y,z) \
